c15860413c4d7ffe8eef53724cc191d2912725b5
MaHi <muhammadshefain123@gmail.com>
2024-07-12 17:38:48 +0600

    added search features

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,6 +4,16 @@ import search_icon from '../assets/search.png';
 const Weather = () => {
   const [weatherData, setWeatherData] = useState('');
 
+  const [city, setCity] = useState('');
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+  const showOnClick = () => {
+    search(city);
+    setCity('');
+  };
+
   const search = async (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
       import.meta.env.VITE_API_KEY
@@ -17,9 +27,8 @@ const Weather = () => {
         location: data.name,
         windSpeed: data.wind.speed,
         temp: data.main.temp,
-        humidity:data.main.humidity,
-        icon: data.weather[0].icon
-
+        humidity: data.main.humidity,
+        icon: data.weather[0].icon,
       });
       console.log('from state', weatherData);
     } catch (e) {
@@ -28,7 +37,7 @@ const Weather = () => {
   };
 
   useEffect(() => {
-    search("Dhaka")
+    search('Dhaka');
   }, []);
 
   return (
@@ -40,15 +49,24 @@ const Weather = () => {
           id=""
           placeholder="search"
           className="h-[50px] rounded-[40px] bg-[#ebfffc] text-[#626262] text-xl pl-6"
+          value={city}
+          required
+          onChange={handleChange}
         />
         <img
           src={search_icon}
           alt=""
           className="w-[50] p-4 rounded-full bg-[#ebfffc] cursor-pointer"
+          onClick={showOnClick}
         />
       </div>
-      <img className="w-[150px] mtb-8 " src={`https://openweathermap.org/img/wn/${weatherData.icon}.png`} />
-      <p className="text-white text-[48px] leading-10 mt-8">{Math.floor(weatherData.temp)}°</p>
+      <img
+        className="w-[150px] mtb-8 "
+        src={`https://openweathermap.org/img/wn/${weatherData.icon}.png`}
+      />
+      <p className="text-white text-[48px] leading-10 mt-8">
+        {Math.floor(weatherData.temp)}°
+      </p>
       <p className="text-[40px] text-white">{weatherData.location}</p>
     </div>
   );
